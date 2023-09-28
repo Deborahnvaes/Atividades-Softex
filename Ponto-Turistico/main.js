3555097427c092847410b875d55e7b105c2cfa1a
Déborah Novaes <110925099+Deborahnvaes@users.noreply.github.com>
2023-09-28 18:04:46 -0300

    atividade de HTML plataforma!

--- Ponto-Turistico/main.js
+++ Ponto-Turistico/main.js
@@ -1,11 +1,10 @@
   function hoverCard(card) {
-    card.style.transform = 'translateY(-10px)';  /* Mova o card para cima */
-    card.style.transition = 'transform 0.3s ease-out';  /* Adicione uma transição suave */
-    card.style.backgroundColor = '#FFA07A	';  /* Altere a cor de fundo quando o mouse está sobre o card */
+    card.style.transform = 'translateY(-10px)'; 
+    card.style.transition = 'transform 0.3s ease-out'; 
+    card.style.backgroundColor = '#FFA07A	';  
   }
 
   function unhoverCard(card) {
-    card.style.transform = 'translateY(0)';  /* Restaure a posição original do card */
-    card.style.transition = 'transform 0.3s ease-out';  /* Adicione uma transição suave */
-    card.style.backgroundColor = '#fff';  /* Restaure a cor de fundo original quando o mouse sai do card */
+    card.style.transform = 'translateY(0)';  
+    card.style.transition = 'transform 0.3s ease-out'; 
   }
